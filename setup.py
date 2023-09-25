33a737784e43e27cced97836552dcb03eafe237b
SAM OCHU <118100214+SAM-OCHU@users.noreply.github.com>
2023-09-25 08:45:33 -0700

    Create setup.py
    this module setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="Amina Ai",
+    version="1.0",
+    description="This module is for Amina Ai",
+    author="SAM OCHU",
+    packages=["Amina Ai"],
+)
